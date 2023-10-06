2549698a6adad3fa7f5a18518dc283cd4c705133
Ashu05122004 <139475265+Ashu05122004@users.noreply.github.com>
2023-10-06 12:48:04 +0530

    Update variables.js

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,1 +1,13 @@
+const account_id = 1234;
+let account_email = "ashu@gmail.com";
+var account_password = "abc123";
+account_city = "jaipur";
+let account_state ;
+
+
+account_email="abbas@gmail.com";
+account_password="3422";
+account_city="lucknow";
+
+console.table([account_id,account_email,account_password,account_city,account_state]);
 
