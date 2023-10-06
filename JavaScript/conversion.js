c33754cb5d9bab08fe702bc1ef45a95020470871
Ashu05122004 <139475265+Ashu05122004@users.noreply.github.com>
2023-10-06 13:50:49 +0530

    Update conversion.js

--- JavaScript/conversion.js
+++ JavaScript/conversion.js
@@ -1,1 +1,22 @@
 
+let str_age = "432";
+console.log(typeof(str_age));
+
+let num_age = Number(str_age);
+console.log(num_age);
+console.log(typeof(num_age));
+
+let name = null;
+console.log(typeof(name));
+
+let name2 = Number(name);
+console.log(name2);
+console.log(typeof(name2));
+
+
+let n = undefined;
+console.log(typeof(n));
+
+let n2 = Number(n);
+console.log(n2);
+console.log(typeof(n2));
